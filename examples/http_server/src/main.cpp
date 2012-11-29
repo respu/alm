ad61306a591f20063aeae434f8bbbd2bffa0d6e9
alvalea <alvalea@gmail.com>
2012-11-29 23:15:41 +0100

    Move responseFile generic method to http_processor class.

--- examples/http_server/src/main.cpp
+++ examples/http_server/src/main.cpp
@@ -4,62 +4,33 @@
 #include "thread_pool.h"
 #include "serverstream.h"
 #include "http_processor.h"
-#include "filereader.h"
-
 
 class processor
 {
 public:
-  processor() :pool(2) { }
+  processor(const char* basedir) :pool(2), base(basedir) { }
 
   ~processor() { }
 
-  void request(int socketFD, const std::string &fileName)
+  void request(int socketFD, const std::string &url)
   {
+    std::string fileName = base + url;
     pool.submit([&, socketFD, fileName]
       {
-        worker(socketFD, fileName);
+        alm::http_processor<processor>::responseFile(socketFD, fileName);
       });
   }
 
-  void worker(int socketFD, const std::string &fileName)
-  {
-    try
-    {
-      alm::filereader reader(fileName.c_str());
-      alm::http_processor<processor>::responseHeader(socketFD, fileName, reader.size());
-      response(socketFD, reader);
-    }
-    catch(alm::file_not_found_exception &e)
-    {
-      alm::http_processor<processor>::notFound(socketFD);
-    }
-  }
-
-  void response(int socketFD, alm::filereader &reader)
-  {
-    unsigned char buffer[reader.size()];
-    reader.read(buffer, reader.size());
-
-    int remainingMessageSize = reader.size(); 
-    unsigned char* position = buffer;
-
-    int rc;
-    while((rc = write(socketFD, position, remainingMessageSize)) > 0)
-    {
-      remainingMessageSize = remainingMessageSize - rc;
-      position += rc;
-    } 
-  } 
-
 private:
   alm::thread_pool pool;
+
+  std::string base;
 };
 
 int main(void)
 {
-  processor p;
-  alm::http_processor<processor> http_p(p, "/home/alem/Workspace/web/");
+  processor p("/home/alem/Workspace/web/");
+  alm::http_processor<processor> http_p(p);
   alm::serverstream<alm::http_processor<processor>> server(1100, http_p);
   server.start();
 
--- include/http_processor.h
+++ include/http_processor.h
@@ -1,10 +1,11 @@
-#ifndef __ALM__HTTP_SERVER__
-#define __ALM__HTTP_SERVER__
+#ifndef __ALM__HTTP_PROCESSOR__
+#define __ALM__HTTP_PROCESSOR__
 
 #include <string>
 #include <sstream>
 #include <map>
 #include "exceptions.h"
+#include "filereader.h"
 
 namespace alm
 {
@@ -13,9 +14,7 @@ template<typename Processor>
 class http_processor
 {
 public:
-  http_processor(Processor &processor, const char* webdir)
-    : m_processor(processor), m_webdir(webdir)
-  { }
+  http_processor(Processor &processor) : m_processor(processor) { }
 
   ~http_processor() { }
 
@@ -42,6 +41,54 @@ public:
       ss.write(m_input, rc);
 
       request(socketFD, ss);
+    }
+  }
+
+  static void responseFile(int socketFD, const std::string &fileName)
+  {
+    try
+    {
+      filereader reader(fileName.c_str());
+      responseHeader(socketFD, fileName, reader.size());
+      sendFile(socketFD, reader);
+    }
+    catch(file_not_found_exception &e)
+    {
+      notFound(socketFD);
+    }
+  }
+
+private:
+  static const int INPUT_SIZE = 8096;
+
+  Processor& m_processor;
+
+  char m_input[INPUT_SIZE];
+
+  static std::map<std::string, std::string> m_extensions;
+
+  void request(int socketFD, std::stringstream &ss)
+  {
+    std::string cmd;
+    ss >> cmd;
+    if(cmd.compare("GET") == 0)
+    {
+      std::string url;
+      ss >> url;
+
+      processRequest(socketFD, url);
+    }
+    else
+    {
+      forbidden(socketFD);
+    }
+  }
+
+  void processRequest(int socketFD, const std::string &url)
+  {
+    if(allowed(socketFD, url))
+    {
+      m_processor.request(socketFD, url);
     }
   }
 
@@ -78,55 +125,33 @@ public:
     write(socketFD, ss.str().c_str(), ss.str().length());
   }
 
-private:
-  static const int INPUT_SIZE = 8096;
-
-  Processor& m_processor;
-
-  std::string m_webdir;
-
-  char m_input[INPUT_SIZE];
-
-  static std::map<std::string, std::string> m_extensions;
-
-  void request(int socketFD, std::stringstream &ss)
-  {
-    std::string cmd;
-    ss >> cmd;
-    if(cmd.compare("GET") == 0)
-    {
-      std::string file;
-      ss >> file;
-
-      processRequest(socketFD, file); 
-    }
-    else
-    {
-      forbidden(socketFD);
-    }
-  }
-
-  void processRequest(int socketFD, const std::string &file)
-  {
-    if(allowed(socketFD, file))
-    {
-      std::string fileName = m_webdir + file;
-
-      m_processor.request(socketFD, fileName); 
-    }
-  }
-
-  bool allowed(int socketFD, const std::string &file)
+  bool allowed(int socketFD, const std::string &url)
   {
     bool result = true;
 
-    if(file.find("..") == 0)
+    if(url.find("..") == 0)
     {
       result = false;
       forbidden(socketFD);
     }
 
     return result;
+  }
+
+  static void sendFile(int socketFD, alm::filereader &reader)
+  {
+    unsigned char buffer[reader.size()];
+    reader.read(buffer, reader.size());
+
+    int remainingMessageSize = reader.size(); 
+    unsigned char* position = buffer;
+
+    int rc;
+    while((rc = write(socketFD, position, remainingMessageSize)) > 0)
+    {
+      remainingMessageSize = remainingMessageSize - rc;
+      position += rc;
+    } 
   }
 };
 
